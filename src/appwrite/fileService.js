4201ffc90a70d1e78ae9a3dc5abbff72056e2350
Muhammad jareer <jareerameer@gmail.com>
2024-09-23 04:02:31 -0700

    post and file methods are configured

---
+++ src/appwrite/fileService.js
@@ -0,0 +1,57 @@
+import { Client, ID, Storage } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+    client = new Client()
+    databases;
+    bucket;
+
+    constructor () {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+            this.bucket = new Storage(this.client)
+    }
+
+    async createFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createFile :: error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error);
+            return false;
+        }
+    }
+
+    filePreview(fileId) {
+        try {
+            return this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: filePreview :: error", error);
+            return false;
+        }
+    }
+}
+
+const service = new Service()
+
+export default service;
