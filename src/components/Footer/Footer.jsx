504ec6173aeeb1184925db017877f2fb43fdcffa
Muhammad jareer <jareerameer@gmail.com>
2024-09-23 21:01:20 -0700

    tailwind configuration and basic comps structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+    return ( 
+        <h1 className="text-2xl text-white bg-green-500 px-3">Footer</h1>
+     );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return ( 
+        <h1 className="text-2xl text-white bg-green-500 px-3">Header</h1>
+     );
+}
+
+export default Header;
