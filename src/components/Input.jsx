fc60ea9b8a78014a8b7817bd73431ec95547bb09
Muhammad jareer <jareerameer@gmail.com>
2024-09-25 10:45:25 -0700

    postcard, login and select setup, react-hhok-form

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -14,9 +14,6 @@ const Input = React.forwardRef(function Input({
             htmlFor={id}>
                 {label}    
             </label>}
-            <Input
-            className
-            />
             <input 
             type={type}
             className={`px-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
